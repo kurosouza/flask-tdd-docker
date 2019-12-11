57e2b31b40338b5f6fe5eab9ca9abff749b47495
Kuro Souza <kurosouza@gmail.com>
2019-12-11 13:08:06 +0100

    Testing - Phase III

--- project/__init__.py
+++ project/__init__.py
@@ -4,33 +4,25 @@ from flask_sqlalchemy import SQLAlchemy
 import sys
 import os
 
-app = Flask(__name__)
+db = SQLAlchemy()
 
-api = Api(app)
+def create_app(script_info=None):
+   app = Flask(__name__)
 
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
+   app_settings = os.getenv('APP_SETTINGS')
+   app.config.from_object(app_settings)
+   
+   db.init_app(app)
 
-db = SQLAlchemy(app)
+   from project.api.ping import ping_blueprint
+   app.register_blueprint(ping_blueprint)
 
-class User(db.Model):
-   __tablename__ = 'users' 
-   id = db.Column(db.Integer, primary_key = True)
-   username = db.Column(db.String(128), nullable = False)
-   email = db.Column(db.String(128), nullable = False)
-   active = db.Column(db.Boolean(), default = True, nullable = False)
+   from project.api.users import users_blueprint
+   app.register_blueprint(users_blueprint)
 
-   def __init__(self, username, email):
-      self.username = username
-      self.email = email
+   @app.shell_context_processor
+   def ctx():
+      return { 'app': app, 'db': db }
 
-class Ping(Resource):
-   def get(self):
-      return {
-         'status': 'success',
-         'message': 'pong!'
-      }
-
-# print(app.config, file=sys.stderr)
-
-api.add_resource(Ping, '/ping')
+   return app
+   
