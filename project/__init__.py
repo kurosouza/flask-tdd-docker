1c65d953e0ff7c14f1d587101a12c0a3283ff5a0
Kuro Souza <kurosouza@gmail.com>
2019-12-11 19:53:45 +0100

    Code coverage and quality.

--- project/__init__.py
+++ project/__init__.py
@@ -6,23 +6,25 @@ import os
 
 db = SQLAlchemy()
 
+
 def create_app(script_info=None):
-   app = Flask(__name__)
+    app = Flask(__name__)
 
-   app_settings = os.getenv('APP_SETTINGS')
-   app.config.from_object(app_settings)
-   
-   db.init_app(app)
+    app_settings = os.getenv("APP_SETTINGS")
+    app.config.from_object(app_settings)
 
-   from project.api.ping import ping_blueprint
-   app.register_blueprint(ping_blueprint)
+    db.init_app(app)
 
-   from project.api.users import users_blueprint
-   app.register_blueprint(users_blueprint)
+    from project.api.ping import ping_blueprint
 
-   @app.shell_context_processor
-   def ctx():
-      return { 'app': app, 'db': db }
+    app.register_blueprint(ping_blueprint)
 
-   return app
-   
+    from project.api.users import users_blueprint
+
+    app.register_blueprint(users_blueprint)
+
+    @app.shell_context_processor
+    def ctx():
+        return {"app": app, "db": db}
+
+    return app
